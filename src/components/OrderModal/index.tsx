f3b72ac8264d1d22e455f2831ef00eead1e04a48
Diego Almeida <diegoalmda@gmail.com>
2022-11-19 15:43:03 -0300

    push waiterapp web

---
+++ src/components/OrderModal/index.tsx
@@ -0,0 +1,101 @@
+import { Overlay, ModalBody, OrderDetails, Actions } from './styles';
+
+import { formatCurrency } from '.././../utils/formatCurrency';
+
+import closeIcon from '../../assets/images/close-icon.svg';
+import { Order } from '../../types/Order';
+import { useEffect } from 'react';
+
+interface OrderModalProps {
+  visible: boolean;
+  order: Order | null;
+  onClose: () => void;
+}
+
+export function OrderModal({ visible, order, onClose }: OrderModalProps) {
+  if(!visible || !order) {
+    return null;
+  }
+
+  const total = order.products.reduce((acc, {product, quantity}) => {
+    return product.price * quantity + acc;
+  }, 0);
+
+  useEffect(() => {
+    function handleKeyDown(event: KeyboardEvent) {
+      if(event.key === 'Escape') {
+        onClose();
+      }
+    }
+    document.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [onClose]);
+
+  return (
+    <Overlay>
+      <ModalBody>
+        <header>
+          <strong>Mesa {order.table}</strong>
+
+          <button type="button" onClick={onClose}>
+            <img src={closeIcon} alt="Ícone fechar modal" />
+          </button>
+        </header>
+
+        <div className="status-container">
+          <small>Status do Pedido</small>
+          <div>
+            <span>
+              {order.status === 'WAITING' && '🕰'}
+              {order.status === 'IN_PRODUCTION' && '👨🏻‍🍳'}
+              {order.status === 'DONE' && '✅'}
+            </span>
+            <strong>
+              {order.status === 'WAITING' && 'Fila de espera'}
+              {order.status === 'IN_PRODUCTION' && 'Em preparação'}
+              {order.status === 'DONE' && 'Pronto!'}
+            </strong>
+          </div>
+        </div>
+
+        <OrderDetails>
+          <strong>Itens</strong>
+          <div className="order-items">
+            {order.products.map(({ _id, product, quantity }) => (
+              <div className="item" key={_id}>
+                <img src={`http://localhost:3001/uploads/${product.imagePath}`} alt={product.name} width="56" height="28.51" />
+
+                <span className="quantity">
+                  {quantity}x
+                </span>
+                <div className="product-details">
+                  <strong>{product.name}</strong>
+                  <span>{formatCurrency(product.price)}</span>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          <div className="total">
+            <span>Total</span>
+            <strong>{formatCurrency(total)}</strong>
+          </div>
+        </OrderDetails>
+
+        <Actions>
+          <button type="button" className="primary">
+            <span>👨🏻‍🍳</span>
+            <strong>Iniciar produção</strong>
+          </button>
+
+          <button type="button" className="secondary">
+            <strong>Cancelar pedido</strong>
+          </button>
+        </Actions>
+      </ModalBody>
+    </Overlay>
+  );
+}
---
+++ src/components/Orders/index.tsx
@@ -0,0 +1,25 @@
+import { orders } from '../../mocks/orders';
+import { OrdersBoard } from '../OrdersBoard';
+import { Container } from './styles';
+
+export function Orders() {
+  return (
+    <Container>
+      <OrdersBoard
+        icon="🕰"
+        title="Fila de espera"
+        orders={orders}
+      />
+      <OrdersBoard
+        icon="👨🏻‍🍳"
+        title="Em preparação"
+        orders={[]}
+      />
+      <OrdersBoard
+        icon="✅"
+        title="Pronto"
+        orders={[]}
+      />
+    </Container>
+  );
+}
